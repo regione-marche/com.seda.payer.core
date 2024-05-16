961313ddcc6af145aa781b122538b68c7178cb10
Dario Licci <dario.licci@maggioli.it>
2024-05-16 10:35:46 +0200

    PGNTCORE-33

--- src/com/seda/payer/core/handler/rest/routine/SPISOGG4.java
+++ src/com/seda/payer/core/handler/rest/routine/SPISOGG4.java
@@ -19,7 +19,12 @@ public class SPISOGG4 extends ARestRoutine {
 		Map<Integer, String> inParameterMap = new HashMap<Integer, String>();
 		
 		inParameterMap.put(p++, "data_in_coduten_in");
-		
+		inParameterMap.put(p++, "data_in_codente_in");
+		inParameterMap.put(p++, "data_in_codimse_in");
+		inParameterMap.put(p++, "data_in_codfunz_in");
+		inParameterMap.put(p++, "data_io_datiniz_in");
+		inParameterMap.put(p++, "data_io_datfine_in");
+
 		return inParameterMap;
 	}
 
@@ -31,14 +36,19 @@ public class SPISOGG4 extends ARestRoutine {
 		Map<Integer, String> outParameterMap = new HashMap<Integer, String>();
 		
 		outParameterMap.put(p++, "data_in_coduten_out");
-		
+		outParameterMap.put(p++, "data_in_codente_out");
+		outParameterMap.put(p++, "data_in_codimse_out");
+		outParameterMap.put(p++, "data_in_codfunz_out");
+		outParameterMap.put(p++, "data_io_datiniz_out");
+		outParameterMap.put(p++, "data_io_datfine_out");
+		outParameterMap.put(p++, "data_ou_retcode_out");
+		outParameterMap.put(p++, "data_ou_message_out");
+
 		return outParameterMap;
 	}
 
 	@Override
 	protected Map<Integer, Map<Integer, String>> resultSetsMap() {
-		
-		// TODO:
 		
 		return null;
 	}
